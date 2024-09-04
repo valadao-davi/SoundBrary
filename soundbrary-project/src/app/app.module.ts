935b3c998c8bae810412d16aba5a4443acd60941
Davi Silva Valadão <davi.raichu@gmail.com>
2024-09-03 21:33:18 -0300

    construção dos arquivos

--- soundbrary-project/src/app/app.module.ts
+++ soundbrary-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { InputComponent } from './screens/login-screen/components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
