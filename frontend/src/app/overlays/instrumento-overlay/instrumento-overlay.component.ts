292c5589cd3ddac4cdb4dae394b9231a9ba6c165
unknown <dev@NSN1191261246.edu119.local>
2024-11-12 10:23:01 -0300

    visualizacao de efeitos e parametros de cada instrumento

--- frontend/src/app/overlays/instrumento-overlay/instrumento-overlay.component.ts
+++ frontend/src/app/overlays/instrumento-overlay/instrumento-overlay.component.ts
@@ -8,4 +8,21 @@ import { Instrument } from 'src/app/layouts/Instrument';
 })
 export class InstrumentoOverlayComponent {
   @Input() instrumentParams!: Instrument;
+  listParams!: Array<{ [key: string]: string }>;
+    nameEffect!: string
+
+    ngOnInit() {
+      const firstEffect = this.instrumentParams.effects ? Object.entries(this.instrumentParams.effects)[0] : null;
+      console.log(this.instrumentParams.effects)
+      console.log(firstEffect)
+      if(firstEffect) {
+        this.loadListParams({ key: firstEffect[0], value: firstEffect[1] });
+      }
+    }
+
+  loadListParams(effect: any){
+    this.nameEffect = effect.key
+    this.listParams = effect.value
+    console.log(this.listParams)
+  }
 }
