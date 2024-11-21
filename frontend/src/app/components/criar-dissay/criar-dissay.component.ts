1f5cd793ca5e8c88671afc4b93685764f03ba02a
kaique-on <kaiqueoliveira.n12@gmail.com>
2024-11-21 09:23:48 -0300

    merge

--- frontend/src/app/components/criar-dissay/criar-dissay.component.ts
+++ frontend/src/app/components/criar-dissay/criar-dissay.component.ts
@@ -1,7 +1,5 @@
-import { Overlay, OverlayRef } from '@angular/cdk/overlay';
-import { TemplatePortal } from '@angular/cdk/portal';
 import { Component, ElementRef, ViewChild, AfterViewInit, TemplateRef, ViewContainerRef, OnDestroy } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { DefaultInstrument } from 'src/app/layouts/DefaultInstrument';
 import { Music } from 'src/app/layouts/Music';
 import { ServiceInstrumentsImageService } from 'src/app/services/service-instruments-image.service';
@@ -12,19 +10,15 @@ import { ServiceMusicService } from 'src/app/services/service-music.service';
   templateUrl: './criar-dissay.component.html',
   styleUrls: ['./criar-dissay.component.css']
 })
-export class CriarDissayComponent implements AfterViewInit, OnDestroy {
+export class CriarDissayComponent {
   @ViewChild('inputElement') inputElement!: ElementRef;
   @ViewChild('searchOverlayComponent') searchOverlayComponent!: TemplateRef<any>;
   
   searchQuery!: string;
   tracksSearched!: Music[]
   private searchSubject: Subject<string> = new Subject<string>();
-  overlayRef!: OverlayRef;
 
-  constructor(private overlay: Overlay,private serviceDefaultImages: ServiceInstrumentsImageService, private serviceSpotify: ServiceMusicService, private viewContainerRef: ViewContainerRef){
-    this.searchSubject.pipe(debounceTime(1000)).subscribe(value => {
-      this.getTracksQuery(value)
-    })
+  constructor(private serviceDefaultImages: ServiceInstrumentsImageService, private serviceSpotify: ServiceMusicService){
   }
   ngAfterViewInit(): void {
     throw new Error('Method not implemented.');
@@ -44,71 +38,12 @@ export class CriarDissayComponent implements AfterViewInit, OnDestroy {
     this.listDefaultInstruments = this.serviceDefaultImages.getDefaultInstruments();
   }
 
-  getTracksQuery(query: string): void {
-    if(query){
-      this.serviceSpotify.getQueryMusic(query).subscribe(items => {
-        this.tracksSearched = items
-        console.log(this.tracksSearched)
-      })
-    }
-  }
-  onSearchChange(value: string) {
-    this.searchQuery = value;
-    this.searchSubject.next(this.searchQuery);
-
-    if (value && !this.overlayRef) {
-      this.openOverlay();
-    } else if (!value && this.overlayRef) {
-      this.closeOverlay();
-    } else if (value && this.overlayRef) {
-      this.closeOverlay();
-      this.openOverlay();
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.overlayRef) {
-      this.overlayRef.dispose();
-    }
-  }
-
-  openOverlay(){
-    const positionStrategy = this.overlay.position()
-    .flexibleConnectedTo(this.inputElement)
-    .withPositions([
-      {
-        originX: 'start',
-        originY: 'bottom',
-        overlayX: 'start',
-        overlayY: 'top'
-      }
-    ])
-
-    this.overlayRef = this.overlay.create({
-      positionStrategy,
-      hasBackdrop: false
-    })
-
-    this.overlayRef.attach(new TemplatePortal(this.searchOverlayComponent, this.viewContainerRef));
-
-    this.overlayRef.backdropClick().subscribe(() => this.closeOverlay());
-  }
-
-  closeOverlay() {
-    if (this.overlayRef) {
-      this.overlayRef.detach();
-    }
-  }
-
-  // Ajusta dinamicamente a altura do textarea conforme o conteúdo
   adjustHeight(textarea: HTMLTextAreaElement) {
-    textarea.style.height = 'auto';  // Reseta a altura para o valor 'auto' antes de recalcular
-    textarea.style.height = `${textarea.scrollHeight}px`;  // Ajusta a altura conforme o conteúdo
+    textarea.style.height = 'auto';  
+    textarea.style.height = `${textarea.scrollHeight}px`;  
   }
 
-  // Função para atualizar os contadores e verificar o limite
   onInput(event: Event, type: string): void {
-    
     const target = event.target as HTMLTextAreaElement | HTMLInputElement;
     if (target) {
       if (type === 'title') {
